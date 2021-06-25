bcc022baf7a56bcdfae24fab12eafb6009e51953
Giannis Kabouris <giannhskabouris@hotmail.com>
2021-06-25 20:13:38 +0300

    validations on book class

--- Omadiko.Entities/Book.cs
+++ Omadiko.Entities/Book.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Omadiko.Entities.Custom_Validations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +11,14 @@ namespace Omadiko.Entities
     class Book
     {
         public int BookId { get; set; }
+        [Required(ErrorMessage = "Book must have a price ")]
+        [CustomValidation(typeof(ValidationMethods), Methods.ValidateGreaterThanZero)]
         public int Price { get; set; }
+        [Required(ErrorMessage = "Book Must Have A Price ")]
+        [MaxLength(25),MinLength(5)]
         public string Title { get; set; }
+        [Required(ErrorMessage = "We must know if book is available or not ")]
         public bool IsAvailable { get; set; }
     }
 }
+    
