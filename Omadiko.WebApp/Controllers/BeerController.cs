8520a3e9542496a9ab7b8b430119b630d8616184
Giannis Kabouris <giannhskabouris@hotmail.com>
2021-07-02 20:28:29 +0300

    new beer controller with new properties

--- Omadiko.WebApp/Controllers/BeerController.cs
+++ Omadiko.WebApp/Controllers/BeerController.cs
@@ -47,7 +47,7 @@ namespace Omadiko.WebApp.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "BeerId,Name,Price,Popularity,Description,PhotoUrl")] Beer beer)
+        public ActionResult Create([Bind(Include = "BeerId,Name,Price,ABV,Color,Type,Popularity,Description,PhotoUrl")] Beer beer)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace Omadiko.WebApp.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "BeerId,Name,Price,Popularity,Description,PhotoUrl")] Beer beer)
+        public ActionResult Edit([Bind(Include = "BeerId,Name,Price,ABV,Color,Type,Popularity,Description,PhotoUrl")] Beer beer)
         {
             if (ModelState.IsValid)
             {
