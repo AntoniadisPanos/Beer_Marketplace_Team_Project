ca8c03312e9d0795758a5ccacee320f774e2db6b
Panos Antoniadis <antoniadis.p1@gmail.com>
2021-07-11 19:33:27 +0300

    changes to user Class. new classes_payment/ShippingDetails/Blog

---
+++ Omadiko.Entities/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omadiko.Entities
+{
+    class Blog
+    {
+        public int BlogId { get; set; }
+        public string Article { get; set; }
+        public int Comments { get; set; }
+    }
+}
